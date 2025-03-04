8ae9f7205cf64035351aefe4e56a66c90df20d6e
tacogips <me@tacogips.me>
2025-03-04 14:22:08 +0900

    some

--- nixos/hm/taco/nixvim/plugins/hop.nix
+++ nixos/hm/taco/nixvim/plugins/hop.nix
@@ -2,41 +2,37 @@
 {
 
   plugins.hop = {
-    keys = "fdjhklsagqwertyuiopzxcvbnm";
-
-    keymaps = {
-      normal = {
-        "f" = {
-          action = "require('hop').hint_char1({ current_line_only = true })";
-          lua = true;
-          silent = true;
-          noremap = true;
-        };
-
-        "r" = {
-          action = "require('hop').hint_char1({})";
-          lua = true;
-          silent = true;
-          noremap = true;
-        };
-      };
-
-      visual = {
-        "f" = {
-          action = "require('hop').hint_char1({ current_line_only = true })";
-          lua = true;
-          silent = true;
-          noremap = true;
-        };
-
-        "r" = {
-          action = "require('hop').hint_char1({})";
-          lua = true;
-          silent = true;
-          noremap = true;
-        };
-      };
+    settings = {
+      keys = "fdjhklsagqwertyuiopzxcvbnm";
     };
 
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "f";
+      action = "require('hop').hint_char1({ current_line_only = true })";
+      lua = true;
+    }
+    {
+      mode = "n";
+      key = "r";
+      action = "require('hop').hint_char1({})";
+      lua = true;
+    }
+    {
+      mode = "v";
+      key = "f";
+      action = "require('hop').hint_char1({ current_line_only = true })";
+      lua = true;
+    }
+    {
+      mode = "v";
+      key = "r";
+      action = "require('hop').hint_char1({})";
+      lua = true;
+    }
+  ];
+
 }
