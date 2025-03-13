0b46a099ab061a4af55d1b4ceb4121b80278480c
tacogips <me@tacogips.me>
2025-03-13 21:26:02 +0900

    vim

--- nixos/hm/taco/fish/default.nix
+++ nixos/hm/taco/fish/default.nix
@@ -21,11 +21,15 @@ in
       set fish_cursor_visual underscore
       set fish_cursor_replace_one underscore
 
-      function fish_user_key_bindings
-          # Execute this once per mode that emacs bindings should be used in
-          fish_default_key_bindings -M insert
-          fish_vi_key_bindings --no-erase insert
-      end
+      fish_vi_key_bindings
+      bind -M insert ctrl-p up-or-search
+      bind -M insert ctrl-n down-or-search
+
+      #function fish_user_key_bindings
+      #    # Execute this once per mode that emacs bindings should be used in
+      #    fish_default_key_bindings -M insert
+      #    fish_vi_key_bindings --no-erase insert
+      #end
 
 
       function fish_mode_prompt
