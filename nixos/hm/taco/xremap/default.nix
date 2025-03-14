b49b29703b7b6f5e2cc0a11962ca059aa20a5d3e
tacogips <me@tacogips.me>
2025-03-14 15:47:49 +0900

    tweak

--- nixos/hm/taco/xremap/default.nix
+++ nixos/hm/taco/xremap/default.nix
@@ -6,44 +6,41 @@
     enable = true;
     withHypr = true;
     config = {
-      keypress_delay_ms = 10;
-      modmap = [
-        {
-          name = "Global";
-          remap = {
-            "CapsLock" = "Esc";
-          };
-        }
-      ];
+      keypress_delay_ms = 20;
+      #modmap = [
+      #  {
+      #    name = "Global";
+      #    remap = {
+      #      "CapsLock" = "Esc";
+      #    };
+      #  }
+      #];
 
       keymap = [
         {
           name = "familier linux";
           remap = {
-            "C-u" = "PAGEUP";
             "C-m" = "enter";
             "C-h" = "backspace";
-            "C-n" = {
-              with_mark = "down";
-            };
-            "C-p" = {
-              with_mark = "up";
-            };
+            #"C-n" = {
+            #  with_mark = "down";
+            #};
+
+            #"C-p" = {
+            #  with_mark = "up";
+            #};
           };
         }
 
-        {
-          name = "browser";
-          application = {
-            only = [
-              "firefox"
-              "brave-browser"
-            ];
-          };
-          remap = {
-            "C-j" = "F6";
-          };
-        }
+        #{
+        #  name = "browser";
+        #  application = {
+        #    only = "firefox";
+        #  };
+        #  remap = {
+        #    "C-j" = "F6";
+        #  };
+        #}
 
       ];
     };
