17c642dba770a9ff1def2122f597c162a62e97db
tacogips <me@tacogips.me>
2025-03-16 23:11:25 +0900

    ssh

--- nixos/hm/taco/ssh/default.nix
+++ nixos/hm/taco/ssh/default.nix
@@ -7,6 +7,13 @@
       "github.com" = {
         identityFile = "~/.ssh/id_ed25519_github";
       };
+
+      mother = {
+        identityFile = "~/.ssh/id_ed25519_mother";
+        hostname = "192.168.11.5";
+        user = "tacogips";
+        port = 23422;
+      };
     };
   };
 }
