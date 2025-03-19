50f651b4129365f839576c8ebc225525eb645c59
tacogips <me@tacogips.me>
2025-03-19 15:44:52 +0900

    bookmark

--- nixos/hm/taco/xdgdesktops/default.nix
+++ nixos/hm/taco/xdgdesktops/default.nix
@@ -1,81 +1,74 @@
 { config, pkgs, ... }:
 
+let
+
+  #xdg.desktopEntries = {
+  #  bookmark1 = {
+  #    name = "bookmark1";
+  #    comment = "open bookmark with browser";
+  #    exec = "${pkgs.writeShellScriptBin "open-url-from-file1" ''
+  #      #!/bin/sh
+  #      URL_FILE_PATH=~/.private/bookmarks/1.txt
+  #      URL=$(${pkgs.coreutils}/bin/cat $URL_FILE_PATH)
+  #
+  #      ${pkgs.brave}/bin/brave "$URL"
+  #    ''}/bin/open-url-from-file1 ";
+  #    icon = "brave";
+  #    terminal = false;
+  #    categories = [
+  #      "Development"
+  #      "Utility"
+  #    ];
+  #  };
+  #}
+
+  bookmarkCount = 10;
+
+  mkBookmarkEntry = num: {
+    name = "bookmark${toString num}";
+    comment = "Open bookmark ${toString num} with browser";
+    exec = "${pkgs.writeShellScriptBin "open-url-from-file${toString num}" ''
+      #!/bin/sh
+      URL_FILE_PATH=~/.private/bookmarks/${toString num}.txt
+      URL=$(${pkgs.coreutils}/bin/cat $URL_FILE_PATH)
+      ${pkgs.brave}/bin/brave "$URL"
+    ''}/bin/open-url-from-file${toString num}";
+    icon = "brave";
+    terminal = false;
+    categories = [
+      "Development"
+      "Utility"
+    ];
+  };
+  range = builtins.genList (x: x + 1) bookmarkCount;
+  bookmarkEntries = builtins.listToAttrs (
+    map (num: {
+      name = "bookmark${toString num}";
+      value = mkBookmarkEntry num;
+    }) range
+  );
+
+in
 {
   # .desktop エントリの作成
-  xdg.desktopEntries = {
-    bookmark1 = {
-      name = "bookmark1";
-      comment = "open bookmark with browser";
-      exec = "${pkgs.writeShellScriptBin "open-url-from-file1" ''
-        #!/bin/sh
-        URL_FILE_PATH=~/.private/bookmarks/1.txt
-        URL=$(${pkgs.coreutils}/bin/cat $URL_FILE_PATH)
-
-        ${pkgs.brave}/bin/brave "$URL"
-      ''}/bin/open-url-from-file1 ";
-      icon = "brave";
-      terminal = false;
-      categories = [
-        "Development"
-        "Utility"
-      ];
-    };
-
-    bookmark2 = {
-      name = "bookmark2";
-      comment = "open bookmark with browser2";
-      exec = "${pkgs.writeShellScriptBin "open-url-from-file" ''
-        #!/bin/sh
-        URL_FILE_PATH=~/.private/bookmarks/2.txt
-        URL=$(${pkgs.coreutils}/bin/cat $URL_FILE_PATH)
-
-        ${pkgs.brave}/bin/brave "$URL"
-      ''}/bin/open-url-from-file2 ";
-      icon = "brave";
-      terminal = false;
-      categories = [
-        "Development"
-        "Utility"
-      ];
-    };
-
-    bookmark3 = {
-      name = "bookmark3";
-      comment = "open bookmark with browser";
-      exec = "${pkgs.writeShellScriptBin "open-url-from-file3" ''
-        #!/bin/sh
-        URL_FILE_PATH=~/.private/bookmarks/3.txt
-        URL=$(${pkgs.coreutils}/bin/cat $URL_FILE_PATH)
-
-        ${pkgs.brave}/bin/brave "$URL"
-      ''}/bin/open-url-from-file3 ";
-      icon = "brave";
-      terminal = false;
-      categories = [
-        "Development"
-        "Utility"
-      ];
-    };
-
-    ## run s
-    #my-script = {
-    #  name = "My Script";
-    #  genericName = "Utility Script";
-    #  comment = "Run my custom script";
-    #  exec = "${pkgs.writeShellScriptBin "my-script" ''
-    #    #!/bin/sh
-    #    echo "Hello, World!"
-    #    ${pkgs.xmessage}/bin/xmessage "Script executed successfully!"
-    #  ''}/bin/my-script";
-    #  icon = "utilities-terminal";
-    #  terminal = true;
-    #  categories = [
-    #    "Utility"
-    #    "System"
-    #  ];
-    #};
-
-  };
+  xdg.desktopEntries = bookmarkEntries;
+  ## run s
+  #my-script = {
+  #  name = "My Script";
+  #  genericName = "Utility Script";
+  #  comment = "Run my custom script";
+  #  exec = "${pkgs.writeShellScriptBin "my-script" ''
+  #    #!/bin/sh
+  #    echo "Hello, World!"
+  #    ${pkgs.xmessage}/bin/xmessage "Script executed successfully!"
+  #  ''}/bin/my-script";
+  #  icon = "utilities-terminal";
+  #  terminal = true;
+  #  categories = [
+  #    "Utility"
+  #    "System"
+  #  ];
+  #};
 
   xdg.mimeApps = {
     enable = true;
