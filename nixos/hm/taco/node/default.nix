1d0439703b14b60e9df8e60601c3755a0a086613
tacogips <me@tacogips.me>
2025-04-15 17:59:43 +0900

    update

--- nixos/hm/taco/node/default.nix
+++ nixos/hm/taco/node/default.nix
@@ -6,26 +6,24 @@
     nodePackages.npm
     nodePackages.pnpm
     nodePackages.yarn
-    # Enables the use of `npx` command
-    nodePackages.npx
   ];
 
-  # Configure npm with defaults
-  home.file.".npmrc".text = ''
-    prefix=${config.home.homeDirectory}/.npm-global
-    cache=${config.home.homeDirectory}/.npm-cache
-    init-author-name=tacogips
-    init-license=MIT
-  '';
+  ## Configure npm with defaults
+  #home.file.".npmrc".text = ''
+  #  prefix=${config.home.homeDirectory}/.npm-global
+  #  cache=${config.home.homeDirectory}/.npm-cache
+  #  init-author-name=tacogips
+  #  init-license=MIT
+  #'';
 
-  # Add npm global bin to PATH
-  home.sessionPath = [
-    "$HOME/.npm-global/bin"
-  ];
+  ## Add npm global bin to PATH
+  #home.sessionPath = [
+  #  "$HOME/.npm-global/bin"
+  #];
 
-  # Setup directory structure
-  home.activation.nodeDirectories = config.lib.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p $HOME/.npm-global
-    mkdir -p $HOME/.npm-cache
-  '';
+  ## Setup directory structure
+  #home.activation.nodeDirectories = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+  #  mkdir -p $HOME/.npm-global
+  #  mkdir -p $HOME/.npm-cache
+  #'';
 }
