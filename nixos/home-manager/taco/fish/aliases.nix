d2f1e897b46ce857781ee88f4b62dc950cac4d02
tacogips <me@tacogips.me>
2025-05-18 11:44:20 +0900

    update

--- nixos/home-manager/taco/fish/aliases.nix
+++ nixos/home-manager/taco/fish/aliases.nix
@@ -2,7 +2,7 @@
 
 {
   ll = "ls -al";
-  update-taco-main = "sudo nixos-rebuild switch --flake ~/nix/nixos#taco-main";
+  update-taco-main = "sudo nixos-rebuild switch --flake ~/nix/nixos/linux#taco-main";
   ppp = "pwd | wl-copy";
   cdp = "cd (wl-paste -n)";
   fa = "fd -H";
