6c22ecca82451d2017b0adca1e74b688fca0de89
tacogips <taco@tacogipss-MacBook-Air.local>
2025-05-18 18:25:25 +0900

    Add Darwin flake and configuration for macOS

---
+++ nixos/darwin/configuration.nix
@@ -0,0 +1,66 @@
+{ pkgs, ... }:
+
+{
+  # System configuration
+  system = {
+    # Set hostname
+    defaults.NSGlobalDomain = {
+      AppleKeyboardUIMode = 3;
+      ApplePressAndHoldEnabled = false;
+      InitialKeyRepeat = 20;
+      KeyRepeat = 1;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      _HIHideMenuBar = false;
+    };
+    
+    # Enable auto-upgrade
+    stateVersion = 4;
+  };
+  
+  # Nix configuration
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    trusted-users = [ "@admin" ];
+  };
+  
+  # Configure fonts
+  fonts.fontDir.enable = true;
+  fonts.fonts = with pkgs; [
+    jetbrains-mono
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  # Enable homebrew
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    onActivation.cleanup = "zap";
+    taps = [];
+    casks = [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Install basic system packages
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    curl
+    wget
+  ];
+
+  # Auto upgrade nix package and the daemon service
+  services.nix-daemon.enable = true;
+
+  # Create /etc/zshrc that loads the nix-darwin environment
+  programs.zsh.enable = true;
+  programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing
+  system.stateVersion = 4;
+}
---
+++ nixos/darwin/flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, darwin, home-manager, ... }@inputs:
+  let
+    system = "aarch64-darwin"; # For Apple Silicon Macs
+    # system = "x86_64-darwin"; # Uncomment for Intel Macs
+    
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    darwinConfigurations = {
+      "taco-mac" = darwin.lib.darwinSystem {
+        inherit system;
+        modules = [
+          # Darwin configuration
+          ./configuration.nix
+          
+          # Home Manager configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.taco = import ./home-manager;
+          }
+        ];
+      };
+    };
+  };
+}
