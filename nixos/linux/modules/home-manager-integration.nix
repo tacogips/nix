c87a84628202f6ba1bd9403b49ba844cb1a6b68a
tacogips <me@tacogips.me>
2025-05-18 11:50:32 +0900

    Remove unused home-manager-integration.nix file

--- nixos/linux/modules/home-manager-integration.nix
+++
@@ -1,9 +0,0 @@
-# This module integrates the shared Home Manager configuration
-{ config, pkgs, ... }:
-
-{
-  imports = [
-    # Point to the new Home Manager configuration
-    ../../home-manager/taco/home.nix
-  ];
-}
