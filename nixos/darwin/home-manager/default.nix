756347ec13968b5da7a79cf38802a61aafa6939f
tacogips <me+darwin@tacogips.me>
2025-05-18 19:42:53 +0900

    update

--- nixos/darwin/home-manager/default.nix
+++ nixos/darwin/home-manager/default.nix
@@ -4,5 +4,6 @@
   # Darwin-specific Home Manager configurations
   imports = [
     ./home.nix # Import Darwin-specific home settings
+    ./alacritty.nix # Darwin-specific Alacritty configuration
   ];
 }
