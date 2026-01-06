bf7ee7b6aef164dc87750100bfa2bf779498e3fe
tacogips <me@tacogips.me>
2026-01-06 18:57:14 +0900

    fix: temporarily disable julia due to gcc 14 build failure in nixpkgs
    1. Objective:
       Work around julia build failure in nixpkgs caused by gcc 14 incompatibility
    
    2. Primary Changes:
       - Commented out julia package (both 1.12.1 and 1.10.10 are affected)
       - Added comprehensive reference URLs for tracking the issue
    
    3. Key Technical Concepts:
       - nixpkgs julia build broken with gcc 14
       - Upstream fix in progress: NixOS/nixpkgs#375959
    
    4. Files and Code Sections:
       - nixos/shared-home-manager/taco/julia/default.nix: Disabled julia, added Hydra, tracking issue, and fix PR URLs
    
    5. Problem Solving:
       Build failure: make: *** [Makefile:79: julia-deps] Error 2 after OpenBLAS build
       Reproduction: Run `nixos-rebuild switch --flake ~/nix/nixos#taco-main` with julia_110 enabled
    
    6. Impact:
       Allows system rebuild to succeed; Julia temporarily unavailable until upstream fix is merged
    
    7. Related Commits:
       Related: 952b673
    
    8. Unresolved TODOs:
       - [ ] Re-enable julia when NixOS/nixpkgs#375959 is merged

--- nixos/shared-home-manager/taco/julia/default.nix
+++ nixos/shared-home-manager/taco/julia/default.nix
@@ -2,8 +2,13 @@
 
 {
   home.packages = with pkgs; [
-    # Using julia_110 (1.10.x LTS) because julia 1.12.1 is currently broken in nixpkgs
-    # See: https://hydra.nixos.org/job/nixpkgs/trunk/julia.x86_64-linux
-    julia_110
+    # Julia is currently broken in nixpkgs due to GCC 14 incompatibility
+    # Both julia (1.12.1) and julia_110 (1.10.10) fail to build
+    # Hydra status:
+    #   - https://hydra.nixos.org/job/nixpkgs/trunk/julia.x86_64-linux
+    #   - https://hydra.nixos.org/job/nixpkgs/trunk/julia_110.x86_64-linux
+    # Tracking issue: https://github.com/NixOS/nixpkgs/issues/388196
+    # Fix PR: https://github.com/NixOS/nixpkgs/pull/375959
+    # julia  # Temporarily disabled: broken in nixpkgs (gcc 14 issue)
   ];
 }
