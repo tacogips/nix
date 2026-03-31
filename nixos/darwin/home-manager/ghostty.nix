8c35280bff23eb44fe49621731bec73cac9b9ac4
tacogips <me+darwin@tacogips.me>
2026-03-31 23:01:40 +0900

    Refactor Ghostty tmux defaults

--- nixos/darwin/home-manager/ghostty.nix
+++ nixos/darwin/home-manager/ghostty.nix
@@ -2,13 +2,11 @@
 
 {
   taco.ghostty = {
-    autoStartTmux = lib.mkForce true;
     fontFamily = lib.mkForce "JetBrainsMono Nerd Font";
     fontSize = lib.mkForce 13;
     extraConfig = lib.mkForce ''
       macos-titlebar-style = hidden
       macos-option-as-alt = true
-      window-decoration = auto
       keybind = cmd+d=ignore
       keybind = cmd+n=ignore
     '';
